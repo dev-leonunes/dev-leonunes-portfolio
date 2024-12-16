f05fbb99214b4b2e614b0fa3e62f67d0c9e42ef8
futuroDevLeo <leonunes07@outlook.com>
2024-12-16 15:57:12 -0300

    feat: add dark and light mode functionality
    - Added functionality to switch between dark and light mode

--- src/script/index.js
+++ src/script/index.js
@@ -1,10 +1,21 @@
-var checkboxMenu = document.getElementById("checkbox-menu");
-var menu = document.getElementById("menu");
+let checkboxMenu = document.getElementById("checkbox-menu");
+let menu = document.getElementById("menu");
+let $html = document.querySelector("html");
+let $themeSlider = document.getElementById("theme-slider");
 
 checkboxMenu.addEventListener("click", function () {
     if (menu.style.display === "flex") {
         menu.style.display = "none";
     } else {
         menu.style.display = "flex";
+    }
+});
+
+$themeSlider.addEventListener("click", function () {
+    $themeSlider.classList.toggle("active");
+    if ($html.classList.contains("light-mode")) {
+        $html.classList.remove("light-mode");
+    } else {
+        $html.classList.add("light-mode");
     }
 });
